c797374367600ba73f6ed5955f7453fa5b8b2019
Rich Saupe <sabadam32@users.noreply.github.com>
2025-02-14 04:05:42 -0600

    Update common.py

--- pages/utility/common.py
+++ pages/utility/common.py
@@ -6,12 +6,3 @@ class Driver:
 
     page: Page = None
     base_url: str = None
-
-    @classmethod
-    def set_driver(cls, page: Page) -> None:
-        cls.page = page
-        cls.page.set_default_timeout(3000)
-
-    @classmethod
-    def set_base_url(cls, url) -> None:
-        cls.base_url = url
